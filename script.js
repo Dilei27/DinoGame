d30f5f5d2b0a6ed79c4c411b700625788c2849a4
Odirlei Alves <66702471+Dilei27@users.noreply.github.com>
2021-02-02 12:33:33 -0300

    Alteração

--- script.js
+++ script.js
@@ -1,66 +1,74 @@
 const dino = document.querySelector('.dino');
 const background = document.querySelector('.background');
+
+
 let isJumping = false;
+let isGameOver = false;
 let position = 0;
+
 
 function handleKeyup(event) {
     if (event.KeyCode === 32) {
-        if (isJumping) {
+        if (!isJumping) {
         jump();
         }
     }
 }
 
-function jump(){
+function jump() {
     isJumping = true;
 
     let upInterval = setInterval(() => {
         if (position >= 150) {
+            //Descendo
             clearInterval(upInterval);
-        //descendo
+        
         let downInterval = setInterval(() >= {
-            if (condition <= 0) {
+            if (position <= 0) {
                 clearInterval (downInterval);
                 isJumping = false; 
             } else {
-            position -= 20;
-            dino.style.botton = position + 'px';
+                 position -= 20;
+                dino.style.bottom = position + 'px';
             }
-        },20);
-          
-        } else {
-        //subindo
-        position += 20;
-        dino.style.botton = position + 'px';
-        }
-    },20);
+        }, 20);        
+    } else {
+      //Subindo
+      position += 20;
+      dino.style.bottom = position + 'px';
+    }
+  }, 20);
 } 
 
-function creatCactus() {
+function createCactus() {
     const cactus = document.createElement('div');
-    left cactusPosition = 1000;
-    left randomTime = Math.random() * 6000;
+    let cactusPosition = 1000;
+    let randomTime = Math.random() * 6000;
+
+    if (isGameOver) return;
 
     cactus.classList.add('cactus');
-    cactus.style.left = 1000 + 'px';
     background.appendChild(cactus);
+    cactus.style.left = cactusPosition + 'px';   
 
-    left leftInterval = setInterval() => {
-              
+    let leftTimer = setInterval(() => {              
         if (cactusPosition < -60) {
-            clearInterval(leftInterval);
+            //Saiu da tela
+            clearInterval(leftTimer);
             background.removeChild(cactus);
-        }else if (cactusPosition > 0 && cactusPosition < 60) {
-         //gameover
-         clearInterval(leftInterval); 
-         document.body.innerHTML = '<h1 class= "Game Over">Fim de Jogo</h1>';  
-        }else {        
-            cactusPosition -= 10;
-            cactus.style.left = cactusPosition + 'px';
-        },20);
+        } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60) {
+         //Game over
+         clearInterval(leftTimer);
+         isGameOver = true; 
+         document.body.innerHTML = '<h1 class= "game- over">Fim de Jogo</h1>';  
+        } else {        
+          cactusPosition -= 10;
+          cactus.style.left = cactusPosition + 'px';
+          }
+        }, 20);
 
         setTimeout(creatCactus, randomTime);                
 }
 
-creatCactus();
-document.addEventListener('Keyup',handleKeyup);
+createCactus();
+document.addEventListener('Keyup', handleKeyup);
